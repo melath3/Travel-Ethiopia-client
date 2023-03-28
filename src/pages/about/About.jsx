b618c09b08297383fb4ebc2866027ed17ea404b1
unwe7 <unwaryesuf@gmail.com>
2023-03-28 12:14:07 +0300

    first commit

---
+++ src/pages/about/About.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+import "./about.css";
+
+
+function About() {
+  return (
+    
+    <div className="about">
+    
+      <h1>Travel Ethiopia</h1>
+      <p>At Travel Ethiopia we want to enable travellers to share their travel story, travel challenges, and over all their experiance to Ethiopias adventourous travel destinations.  
+       Our mission is to help travellers get insight about differemt tourist destinations from other travellers  experiance in Ethiopia. This enables them to get decide quickly, saves their time, and cost. </p>
+    
+    </div>
+    
+  )
+}
+
+export default About
