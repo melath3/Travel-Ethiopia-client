cbbd57f7f32446c3b959a84904392ac7067e9f55
unwe7 <unwaryesuf@gmail.com>
2023-03-28 13:11:45 +0300

    config & .env file added

--- config.js
+++ config.js
@@ -1,3 +1,3 @@
 import axios from 'axios'
 
-export const axiosInstance= axios.create({baseURL : 'http://localhost:5000/api/'})
+export const axiosInstance= axios.create({baseURL : process.env.REACT_APP_API_PATH})
